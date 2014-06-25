840be7f05fd5ac70465c668197c2fff1322759db
boxsome <jan.castor14@gmail.com>
2014-06-24 22:05:54 -0400

    Changed twitterTest and profileTest samples
    Changed it to fit more with a user scenario for presentation

--- app.js
+++ app.js
@@ -29,17 +29,11 @@ app.get('/twitterTest', function (req, res) {
     res.type('application/json');
     res.json([
         {
-            "created_at": "Tue Mar 21 20:50:14 +0000 2006",
+            "created_at": "Tue Jun 25 09:50:14 +0000 2014",
             "id": 20,
             "id_str": "20",
-            "text": "just setting up my twttr",
+            "text": "Game is being shown at SLC! #Waterloo #WorldCup",
             "source": "web",
-            "truncated": false,
-            "in_reply_to_status_id": null,
-            "in_reply_to_status_id_str": null,
-            "in_reply_to_user_id": null,
-            "in_reply_to_user_id_str": null,
-            "in_reply_to_screen_name": null,
             "user": {
                 "id": 12,
                 "id_str": "12",
@@ -53,153 +47,139 @@ app.get('/twitterTest', function (req, res) {
                         "urls": []
                     }
                 },
-                "protected": false,
-                "followers_count": 2577282,
-                "friends_count": 1085,
-                "listed_count": 23163,
-                "created_at": "Tue Mar 21 20:50:14 +0000 2006",
-                "favourites_count": 2449,
-                "utc_offset": -25200,
-                "time_zone": "Pacific Time (US & Canada)",
-                "geo_enabled": true,
-                "verified": true,
-                "statuses_count": 14447,
                 "lang": "en",
-                "contributors_enabled": false,
-                "is_translator": false,
-                "is_translation_enabled": false,
-                "profile_background_color": "EBEBEB",
-                "profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
-                "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
-                "profile_background_tile": false,
                 "profile_image_url": "http://pbs.twimg.com/profile_images/448483168580947968/pL4ejHy4_normal.jpeg",
                 "profile_image_url_https": "https://pbs.twimg.com/profile_images/448483168580947968/pL4ejHy4_normal.jpeg",
-                "profile_banner_url": "https://pbs.twimg.com/profile_banners/12/1347981542",
-                "profile_link_color": "990000",
-                "profile_sidebar_border_color": "DFDFDF",
-                "profile_sidebar_fill_color": "F3F3F3",
-                "profile_text_color": "333333",
-                "profile_use_background_image": true,
-                "default_profile": false,
-                "default_profile_image": false,
-                "following": true,
-                "follow_request_sent": false,
-                "notifications": false
             },
-            "geo": null,
-            "coordinates": null,
-            "place": null,
-            "contributors": null,
-            "retweet_count": 23936,
-            "favorite_count": 21879,
-            "entities": {
-                "hashtags": [],
-                "symbols": [],
-                "urls": [],
-                "user_mentions": []
-            },
-            "favorited": false,
-            "retweeted": false,
             "lang": "en"
         },
         {
-            "created_at": "Sun Feb 09 23:25:34 +0000 2014",
-            "id": 432656548536401920,
-            "id_str": "432656548536401920",
-            "text": "POST statuses/update. Great way to start. https://t.co/9S8YO69xzf (disclaimer, this was not posted via the API).",
+            "created_at": "Tue Jun 25 08:59:34 +0000 2014",
+            "id": 432656548536401930,
+            "id_str": "432656548536401930",
+            "text": "Oh no.....",
             "source": "web",
-            "truncated": false,
-            "in_reply_to_status_id": null,
-            "in_reply_to_status_id_str": null,
-            "in_reply_to_user_id": null,
-            "in_reply_to_user_id_str": null,
-            "in_reply_to_screen_name": null,
             "user": {
-                "id": 2244994945,
-                "id_str": "2244994945",
-                "name": "TwitterDev",
-                "screen_name": "TwitterDev",
+                "id": 2244994951,
+                "id_str": "2244994951",
+                "name": "Joshy Josh",
+                "screen_name": "NoName",
                 "location": "Internet",
-                "description": "Developers and Platform Relations @Twitter. We are developers advocates. We can't answer all your questions, but we listen to all of them!",
+                "description": "My first and last names are the same names!",
                 "url": "https://t.co/66w26cua1O",
-                "entities": {
-                    "url": {
-                        "urls": [
-                            {
-                                "url": "https://t.co/66w26cua1O",
-                                "expanded_url": "https://dev.twitter.com/",
-                                "display_url": "dev.twitter.com",
-                                "indices": [
-                                    0,
-                                    23
-                                ]
-                            }
-                        ]
-                    },
-                    "description": {
-                        "urls": []
-                    }
-                },
-                "protected": false,
-                "followers_count": 3147,
-                "friends_count": 909,
-                "listed_count": 53,
-                "created_at": "Sat Dec 14 04:35:55 +0000 2013",
-                "favourites_count": 61,
-                "utc_offset": -25200,
-                "time_zone": "Pacific Time (US & Canada)",
-                "geo_enabled": false,
-                "verified": true,
-                "statuses_count": 217,
+                "created_at": "Sat Dec 19 04:35:55 +0000 2013",
                 "lang": "en",
-                "contributors_enabled": false,
-                "is_translator": false,
-                "is_translation_enabled": false,
-                "profile_background_color": "FFFFFF",
-                "profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
-                "profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
-                "profile_background_tile": false,
                 "profile_image_url": "http://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png",
-                "profile_image_url_https": "https://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png",
-                "profile_banner_url": "https://pbs.twimg.com/profile_banners/2244994945/1391977747",
-                "profile_link_color": "0084B4",
-                "profile_sidebar_border_color": "FFFFFF",
-                "profile_sidebar_fill_color": "DDEEF6",
-                "profile_text_color": "333333",
-                "profile_use_background_image": false,
-                "default_profile": false,
-                "default_profile_image": false,
-                "following": true,
-                "follow_request_sent": false,
-                "notifications": false
+                "profile_image_url_https": "https://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png"
             },
-            "geo": null,
-            "coordinates": null,
-            "place": null,
-            "contributors": null,
-            "retweet_count": 1,
-            "favorite_count": 5,
-            "entities": {
-                "hashtags": [],
-                "symbols": [],
-                "urls": [
-                    {
-                        "url": "https://t.co/9S8YO69xzf",
-                        "expanded_url": "https://dev.twitter.com/docs/api/1.1/post/statuses/update",
-                        "display_url": "dev.twitter.com/docs/api/1.1/p…",
-                        "indices": [
-                            42,
-                            65
-                        ]
-                    }
-                ],
-                "user_mentions": []
+            "lang": "en"
+        },
+        {
+            "created_at": "Sun Feb 09 8:55:34 +0000 2014",
+            "id": 432656548536401932,
+            "id_str": "432656548536401932",
+            "text": "Wow this game is so good! #WorldCup",
+            "source": "web",
+            "user": {
+                "id": 2244994952,
+                "id_str": "2244994952",
+                "name": "Jane Smith",
+                "screen_name": "JaneSmith1234",
+                "location": "Internet",
+                "description": "Just another girl!",
+                "url": "https://t.co/66w26cua1O",
+                "created_at": "Sat Dec 14 04:35:55 +0000 2013",
+                "lang": "en",
+                "profile_image_url": "http://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png",
+                "profile_image_url_https": "https://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png"
             },
-            "favorited": false,
-            "retweeted": false,
-            "possibly_sensitive": false,
+            "lang": "en"
+        },
+        {
+            "created_at": "Sun Feb 09 8:50:34 +0000 2014",
+            "id": 432656548536401931,
+            "id_str": "432656548536401931",
+            "text": "I wonder how long it took to organize all of this...",
+            "source": "web",
+            "user": {
+                "id": 2244994951,
+                "id_str": "2244994951",
+                "name": "Smith smit",
+                "screen_name": "SmithDaSmith",
+                "location": "Internet",
+                "description": "I'm just your average smith named Smith!",
+                "url": "https://t.co/66w26cua1O",
+                "created_at": "Sat Dec 14 04:35:55 +0000 2013",
+                "lang": "en",
+                "profile_image_url": "http://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png",
+                "profile_image_url_https": "https://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png"
+            },
+            "lang": "en"
+        },
+        {
+            "created_at": "Tue Jun 25 08:45:34 +0000 2014",
+            "id": 432656548536401922,
+            "id_str": "432656548536401922",
+            "text": "This game right now!?!?!?!?!?!??!!??! #WorldCup",
+            "source": "web",
+            "user": {
+                "id": 2244994947,
+                "id_str": "2244994947",
+                "name": "Daniela A",
+                "screen_name": "DaA",
+                "location": "Internet",
+                "description": "I sing and stuff!",
+                "url": "https://t.co/66w26cua1O",
+                "created_at": "Sat Dec 21 04:35:55 +0000 2013",
+                "lang": "en",
+                "profile_image_url": "http://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png",
+                "profile_image_url_https": "https://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png"
+            },
+            "lang": "en"
+        },
+        {
+            "created_at": "Tue Jun 25 8:25:34 +0000 2014",
+            "id": 432656548536401929,
+            "id_str": "432656548536401929",
+            "text": "Everyone ready for the game? #WorldCup",
+            "source": "web",
+            "user": {
+                "id": 2244994949,
+                "id_str": "2244994949",
+                "name": "Jeffrey Smith",
+                "screen_name": "JeffreySmith1234",
+                "location": "Internet",
+                "description": "Sucks at twitter-ing!",
+                "url": "https://t.co/66w26cua1O",
+                "created_at": "Sat Dec 21 04:35:55 +0000 2013",
+                "lang": "en",
+                "profile_image_url": "http://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png",
+                "profile_image_url_https": "https://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png"
+            },
+            "lang": "en"
+        },
+        {
+            "created_at": "Tue Jun 25 07:25:34 +0000 2014",
+            "id": 432656548536401921,
+            "id_str": "432656548536401921",
+            "text": "Wow this game is so good! #WorldCup",
+            "source": "web",
+            "user": {
+                "id": 2244994946,
+                "id_str": "2244994946",
+                "name": "John Doe",
+                "screen_name": "TheRealJohnDoe",
+                "location": "Internet",
+                "description": "Just another dude!",
+                "url": "https://t.co/66w26cua1O",
+                "created_at": "Sat Dec 19 04:35:55 +0000 2013",
+                "lang": "en",
+                "profile_image_url": "http://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png",
+                "profile_image_url_https": "https://pbs.twimg.com/profile_images/431949550836662272/A6Ck-0Gx_normal.png"
+            },
             "lang": "en"
         }
+
     ]);
 });
 
@@ -212,15 +192,22 @@ app.get('/profileTest', function (req, res) {
         "feed": [
             {
                 "feed_source":1,
-                "author":"GPSocial",
-                "message":"first Twitter post",
+                "author":"Joe Smith",
+                "message":"Who's watching the game? #WorldCup",
                 "created_at":1403531574529,
                 "profile_img_url":"http://lautechstudents.com/images/profile.png"
             },
             {
                 "feed_source": 1,
-                "author": "jack",
-                "message":"Hi I'm Jack",
+                "author": "Joe Smith",
+                "message":"Go Argentina! #Argentina",
+                "created_at":1403531412241,
+                "profile_img_url":"http://lautechstudents.com/images/profile.png"
+            },
+            {
+                "feed_source": 1,
+                "author": "Joe Smith",
+                "message":"I wish I was watching the game right now...........",
                 "created_at":1403531412241,
                 "profile_img_url":"http://lautechstudents.com/images/profile.png"
             }
