dc7bbbadf10867f0f35a543614b5b4f712664877
boxsome <jan.castor14@gmail.com>
2014-08-26 08:29:22 -0400

    Quickly added CORS support

--- app.js
+++ app.js
@@ -44,6 +44,20 @@ var oauth = new OAuth.OAuth(
     'HMAC-SHA1'
 );
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 connection.connect(function(err) {
    if (err) {
        throw err;
@@ -61,6 +75,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(allowCrossDomain);
 
 app.use('/', routes);
 app.use('/users', users);
