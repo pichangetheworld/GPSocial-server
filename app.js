8a666b8dfffaa302e10559d7e041d5665a75e8ee
boxsome <jan.castor14@gmail.com>
2014-08-26 08:06:13 -0400

    Just using for some miscellaneous testing purposes

--- app.js
+++ app.js
@@ -1049,6 +1049,14 @@ app.post('/post_message', function(req, res) {
 
 	
 });
+
+app.post('/sectorSubmit', function(req, res) {
+
+    res.type('application/json');
+    res.send(req.body);
+
+});
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
